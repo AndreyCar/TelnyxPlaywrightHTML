f4f687620d9c6d5f3cb774c72ccc82dd6882f294
AndreyCar <kravchenko.svg@gmail.com>
2022-08-04 16:55:11 +0300

    up

--- helper/account.helper.js
+++ helper/account.helper.js
@@ -1,13 +1,14 @@
 const { Page } = require('../pages/page');
 const { LogInPage } = require('../pages/logIn.page');
 const { HomePage } = require('../pages/homePage/home.page');
+const { expect } = require('@playwright/test');
 
 class Account extends Page {
 	constructor(page) {
 		super(page);
 		this.page = page;
 	}
-	
+
 	homePage = new HomePage(this.page);
 	logInPage = new LogInPage(this.page);
 
@@ -17,6 +18,7 @@ class Account extends Page {
 		await this.homePage.click(this.homePage.logInButton);
 		await this.page.waitForLoadState('load');
 		await this.logInPage.login(email, password);
+		await expect(this.page).toHaveURL(/\/app/, {timeout: 15000});
 	}
 }
 
