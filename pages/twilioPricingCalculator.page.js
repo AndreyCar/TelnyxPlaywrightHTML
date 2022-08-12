04f65b8b3072e323b0a65dc3d1b76e196f6add77
AndreyCar <kravchenko.svg@gmail.com>
2022-08-12 16:38:24 +0300

    up

--- pages/twilioPricingCalculator.page.js
+++ pages/twilioPricingCalculator.page.js
@@ -1,3 +1,4 @@
+const { expect } = require('@playwright/test');
 const { Page } = require('./page');
 
 class TwilioPricingCalculatorPage extends Page {
@@ -11,6 +12,9 @@ class TwilioPricingCalculatorPage extends Page {
 	savingPriceTexts = '//p//span[contains(text(),"$")]';
 	goBackButton = '(//main//header//p)[1]';
 	productsLableButton = 'div[class="Text-sc-5o8owa-0 sc-7a587bdc-1 knOJT dBluGE"]';
+	subtractButtons = '//main//input//preceding-sibling::button';
+	volumeInput = '//input//following-sibling::button//preceding-sibling::input';
+	additionButtons = '//main//input//following-sibling::button';
 
 	constructor(page) {
 		super(page);
@@ -21,8 +25,27 @@ class TwilioPricingCalculatorPage extends Page {
 		super.goto(`twilio-pricing-calculator`);
 	}
 
-	async replaceFromCostByIndex(element, index) {
+	async priceToIntByIndex(element, index) {
 		return parseInt((await this.textContentByIndex(element, index)).replace('$', '').replace(',', ''), 10);
+	}
+
+	async clickAndGetTextByIndex(index) {
+		await this.clickByIndex(this.productsButtons, index);
+		return await this.textContentByIndex(this.productsButtons, index);
+	}
+
+	async checkSubtractAndAdditionButtons() {
+		const inputCount = await this.count(this.volumeInput);
+		let value = 0;
+		for (let index = 0; index < inputCount; index++) {
+			value = parseInt(await this.getAttributeByIndex(this.volumeInput, index, 'value'), 10);
+			await this.clickByIndex(this.subtractButtons, index);
+			await expect(parseInt(await this.getAttributeByIndex(this.volumeInput, index, 'value'), 10)).toBeLessThan(
+				value
+			);
+			await this.clickByIndex(this.additionButtons, index);
+			await expect(parseInt(await this.getAttributeByIndex(this.volumeInput, index, 'value'), 10)).toEqual(value);
+		}
 	}
 }
 
--- tests/twilioPricingCalculator.spec.js
+++ tests/twilioPricingCalculator.spec.js
@@ -16,9 +16,56 @@ test.describe('Savings calculator Functionality', () => {
 		await twilioPricingCalculatorPage.fill(twilioPricingCalculatorPage.websiteInput, 'https://test.com/');
 		await twilioPricingCalculatorPage.click(twilioPricingCalculatorPage.submitButton);
 		expect(
-			await twilioPricingCalculatorPage.replaceFromCostByIndex(twilioPricingCalculatorPage.savingPriceTexts, 0)
-		).toEqual(
-			await twilioPricingCalculatorPage.replaceFromCostByIndex(twilioPricingCalculatorPage.savingPriceTexts, 1)
-		);
+			await twilioPricingCalculatorPage.priceToIntByIndex(twilioPricingCalculatorPage.savingPriceTexts, 0)
+		).toEqual(await twilioPricingCalculatorPage.priceToIntByIndex(twilioPricingCalculatorPage.savingPriceTexts, 1));
+	});
+
+	test('Should check if the selected produts are displayed on the last page', async ({
+		twilioPricingCalculatorPage,
+		page,
+	}) => {
+		let productsName = [];
+		for (let i = 0; i < 7; i++) {
+			if (i < 3) {
+				productsName.push(await twilioPricingCalculatorPage.clickAndGetTextByIndex(i));
+			} else if (i == 3 || i == 4) {
+				productsName.push(await twilioPricingCalculatorPage.clickAndGetTextByIndex(i - 3));
+				productsName.push(await twilioPricingCalculatorPage.clickAndGetTextByIndex(i - 3 + 1));
+			} else if (i == 5) {
+				productsName.push(await twilioPricingCalculatorPage.clickAndGetTextByIndex(0));
+				productsName.push(await twilioPricingCalculatorPage.clickAndGetTextByIndex(2));
+			} else if (i == 6) {
+				productsName.push(await twilioPricingCalculatorPage.clickAndGetTextByIndex(0));
+				productsName.push(await twilioPricingCalculatorPage.clickAndGetTextByIndex(1));
+				productsName.push(await twilioPricingCalculatorPage.clickAndGetTextByIndex(2));
+			}
+			await twilioPricingCalculatorPage.click(twilioPricingCalculatorPage.continueButton);
+			await twilioPricingCalculatorPage.click(twilioPricingCalculatorPage.continueButton);
+			for (let j = 0; j < productsName.length; j++) {
+				await expect(productsName[j]).toMatch(
+					RegExp(
+						await twilioPricingCalculatorPage.textContentByIndex(
+							twilioPricingCalculatorPage.productsLableButton,
+							j
+						),
+						'i'
+					)
+				);
+			}
+			await twilioPricingCalculatorPage.goto();
+			productsName = [];
+			await page.waitForTimeout(1000);
+		}
+	});
+
+	test.only('Should check if the subtract and addition buttons work correct', async ({
+		twilioPricingCalculatorPage,
+	}) => {
+		for (let index = 0; index < 3; index++)
+			await twilioPricingCalculatorPage.clickByIndex(twilioPricingCalculatorPage.productsButtons, index);
+		await twilioPricingCalculatorPage.click(twilioPricingCalculatorPage.continueButton);
+		await twilioPricingCalculatorPage.checkSubtractAndAdditionButtons();
+		await twilioPricingCalculatorPage.click(twilioPricingCalculatorPage.continueButton);
+		await twilioPricingCalculatorPage.checkSubtractAndAdditionButtons();
 	});
 });
